b3e65f0f8538efd05ac52de3fe88a68fb77a00b0
Diego Matesanz <diegomatesanzfuente@gmail.com>
2023-08-02 13:24:06 +0200

    Added app colors and theme

--- app/src/main/java/com/diego/matesanz/rickandmorty/screens/MainActivity.kt
+++ app/src/main/java/com/diego/matesanz/rickandmorty/screens/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.diego.matesanz.rickandmorty.screens
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 import com.diego.matesanz.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            WindowInsetsControllerCompat(window, decorView).isAppearanceLightStatusBars = true
+            statusBarColor = Color.TRANSPARENT
+        }
     }
 }
